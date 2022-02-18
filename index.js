7296d0c90d879c9490884df0bc730f77c7addf5b
Albert Byrone <albertbyrone1677@gmail.com>
2022-02-18 18:08:18 +0300

    submit assignemt

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution here!
+let cats=  ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+function appendCat(name){
+    return cats.concat(name)
+}
+function destructivelyPrependCat(){
+    return cats.slice(0)
+}
